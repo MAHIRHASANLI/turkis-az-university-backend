b2548bcf06552c33c7ecce1a22cb555753b4299a
Mahir Hasanli <126595932+MAHIRHASANLI@users.noreply.github.com>
2025-01-26 20:16:14 +0300

    .

--- server.js
+++ server.js
@@ -4,23 +4,29 @@ const mongoose = require("mongoose");
 const body_parser = require("body-parser");
 require("dotenv").config();
 const app = express();
+
 app.use(cors());
 app.use(body_parser.json());
 
-const info_router = require("./routers/info_router")
+// Import the router for "informations"
+const info_router = require("./routers/info_router");
 
+// Base URL route
 app.get('/', (req, res) => {
-     res.status(200).send('Base URL sorğusuna cavab: Express server uğurla işləyir!');
+     res.status(200).send('Base URL request: Express server is working successfully!');
 });
 
+// Use the "/informations" route
 app.use("/informations", info_router);
 
-app.listen(process.env.PORT, () => {
-     console.log(`Connected PORT: ${process.env.PORT}`);
-});
+// MongoDB connection
+mongoose.connect(process.env.MY_CONNECT)
+     .then(() => {
+          console.log("MongoDB Connected");
+     })
+     .catch((error) => {
+          console.log(error);
+     });
 
-mongoose.connect(process.env.MY_CONNECT).then(() => {
-     console.log("MongoDB Connected");
-}).catch((error) => {
-     console.log(error);
-})
+// Export the Express app to be used by Vercel serverless function
+module.exports = app;
